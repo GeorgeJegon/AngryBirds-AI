c022faffa7c609b8313f9b82b938dc9ce503e992
George Jegon <george_jegon@hotmail.com>
2015-10-01 01:02:46 -0300

    Extends Agent class and remove common methods implementation

--- src/ab/ai/agents/RandomAgent.java
+++ src/ab/ai/agents/RandomAgent.java
@@ -5,123 +5,85 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import ab.ai.BestShot;
 import ab.ai.ListBestShots;
 import ab.ai.Util;
 import ab.demo.other.ActionRobot;
 import ab.demo.other.Shot;
-import ab.planner.TrajectoryPlanner;
 import ab.utils.StateUtil;
 import ab.vision.ABObject;
 import ab.vision.GameStateExtractor.GameState;
 import ab.vision.Vision;
 
-public class RandomAgent implements Runnable {
-	private ActionRobot aRobot;
-	private Random randomGenerator;
-	public int currentLevel = 1;
-	public static int time_limit = 12;
-	private TrajectoryPlanner tp;
-	private boolean firstShot;
-	private Point prevTarget;
+public class RandomAgent extends Agent implements Runnable {
 	private int shotNumber = 0;
-
 	private ListBestShots bestShots = new ListBestShots();
-	private List<Shot> listObjects = null;
+	private static final String BEST_SHOTS_FILE = "bestshots.xml";
 
 	public RandomAgent() {
-		aRobot = new ActionRobot();
-		tp = new TrajectoryPlanner();
-		prevTarget = null;
-		firstShot = true;
-		randomGenerator = new Random();
-		bestShots = loadKnowledge();
-
+		super();
+		bestShots = loadBestShots();
 		ActionRobot.GoFromMainMenuToLevelSelection();
 	}
+	
+	@Override
+	protected void beforeRestartLevel() {
 
-	public void saveKnowledge() {
-		Util.saveXML(bestShots, "bestshots.xml");
 	}
 
-	public ListBestShots loadKnowledge() {
-		ListBestShots loaded = (ListBestShots) Util.loadXML(ListBestShots.class,
-				"bestshots.xml");
-		return (loaded != null) ? loaded : new ListBestShots();
+	@Override
+	protected void afterRestartLevel() {
+
 	}
+	
+	@Override
+	protected void beforeLoadNextLevel() {
+		int score = StateUtil.getScore(ActionRobot.proxy);
+		BestShot bs = new BestShot(currentLevel, score, listObjects);
 
-	// run the client
-	public void run() {
-		listObjects = new ArrayList<Shot>();
-		aRobot.loadLevel(currentLevel);
-
-		while (true) {
-			System.out.println("Execute level " + currentLevel);
-
-			GameState state = solve();
-			if (state == GameState.WON) {
-				int score = StateUtil.getScore(ActionRobot.proxy);
-				// Create a new bestshot.
-				BestShot bs = new BestShot(currentLevel, score, listObjects);
-
-				// If is a new level, add to bestShots list.
-				if (bestShots.size() == currentLevel - 1) {
-					bestShots.add(bs);
-					saveKnowledge();
-				} else { // Update with the new score.
-					if (score > bestShots.get(currentLevel - 1).getScore()) {
-						bestShots.set(currentLevel - 1, bs);
-						saveKnowledge();
-					}
-				}
-
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-
-				aRobot.loadLevel(++currentLevel);
-				tp = new TrajectoryPlanner();
-				firstShot = true;
-
-				listObjects = new ArrayList<Shot>();
-				shotNumber = 0;
-			} else if (state == GameState.LOST) {
-				listObjects = new ArrayList<Shot>();
-				shotNumber = 0;
-				System.out.println("Restart");
-				aRobot.restartLevel();
-			} else if (state == GameState.LEVEL_SELECTION) {
-				System.out.println("Unexpected level selection page: "
-						+ currentLevel);
-				aRobot.loadLevel(currentLevel);
-			} else if (state == GameState.MAIN_MENU) {
-				System.out
-						.println("Unexpected main menu page: " + currentLevel);
-				ActionRobot.GoFromMainMenuToLevelSelection();
-				aRobot.loadLevel(currentLevel);
-			} else if (state == GameState.EPISODE_MENU) {
-				System.out.println("Unexpected episode menu page: "
-						+ currentLevel);
-				ActionRobot.GoFromMainMenuToLevelSelection();
-				aRobot.loadLevel(currentLevel);
+		// If is a new level, add to bestShots list.
+		if (bestShots.size() == currentLevel - 1) {
+			bestShots.add(bs);
+			saveBestShots();
+		} else { // Update with the new score.
+			if (score > bestShots.get(currentLevel - 1).getScore()) {
+				bestShots.set(currentLevel - 1, bs);
+				saveBestShots();
 			}
 		}
 	}
+	
+	@Override
+	protected void afterLoadNextLevel() {
+		this.listObjects = new ArrayList<Shot>();
+		this.shotNumber = 0;
+	}
+
+	public ListBestShots loadKnowledge() {
+		ListBestShots loaded = (ListBestShots) Util.loadXML(
+				ListBestShots.class, BEST_SHOTS_FILE);
+		return (loaded != null) ? loaded : new ListBestShots();
+	}
+
+	public boolean hasBestShot() {
+		BestShot bs = null;
+		if (this.bestShots.size() > this.currentLevel - 1) {
+			bs = bestShots.get(currentLevel - 1);
+			return bs.getShots().size() > shotNumber;
+		}
+		return false;
+	}
 
 	public GameState solve() {
-		
-		boolean hasBestShot = bestShots.size() > currentLevel - 1
-				&& bestShots.get(currentLevel - 1) != null
-				&& bestShots.get(currentLevel - 1).getShots().size() > shotNumber;
+		boolean hasBestShot = this.hasBestShot();
 
 		ABObject abObject = null;
 		BufferedImage screenshot = ActionRobot.doScreenShot();
 		Vision vision = new Vision(screenshot);
 		Rectangle sling = vision.findSlingshotMBR();
+		List<ABObject> objects = new ArrayList<ABObject>();
+		List<Shot> shots = new ArrayList<Shot>();
 
 		while (sling == null && aRobot.getState() == GameState.PLAYING) {
 			System.out
@@ -131,9 +93,6 @@ public class RandomAgent implements Runnable {
 			vision = new Vision(screenshot);
 			sling = vision.findSlingshotMBR();
 		}
-
-		List<ABObject> objects = new ArrayList<ABObject>();
-		List<Shot> shots = new ArrayList<Shot>();
 
 		if (hasBestShot) {
 			// Use the shots that learn to pass the level.
@@ -167,6 +126,7 @@ public class RandomAgent implements Runnable {
 				}
 
 				state = executeShot(sling, shot, state, releasePoint);
+
 			}
 		}
 		return state;
@@ -183,135 +143,19 @@ public class RandomAgent implements Runnable {
 		return objects.subList(0, 1);
 	}
 
-	public Point getReleasePoint(Rectangle sling, Point _tpt) {
-		Point releasePoint = null;
-		// estimate the trajectory
-		ArrayList<Point> pts = tp.estimateLaunchPoint(sling, _tpt);
-
-		// do a high shot when entering a level to find an accurate velocity
-		if (firstShot && pts.size() > 1) {
-			releasePoint = pts.get(1);
-		} else if (pts.size() == 1) {
-			releasePoint = pts.get(0);
-		} else if (pts.size() == 2) {
-			// randomly choose between the trajectories, with a 1 in
-			// 6 chance of choosing the high one
-			if (randomGenerator.nextInt(6) == 0)
-				releasePoint = pts.get(1);
-			else
-				releasePoint = pts.get(0);
-		} else {
-			if (pts.isEmpty()) {
-				System.out.println("No release point found for the target");
-				System.out.println("Try a shot with 45 degree");
-				releasePoint = tp.findReleasePoint(sling, Math.PI / 4);
-			}
-		}
-		return releasePoint;
-	}
-
-	public int getTapTime(Rectangle sling, Point releasePoint, Point _tpt) {
-		int tapInterval = 0;
-		switch (aRobot.getBirdTypeOnSling()) {
-		case RedBird:
-			tapInterval = 0;
-			break; // start of trajectory
-		case YellowBird:
-			tapInterval = 65 + randomGenerator.nextInt(25);
-			break; // 65-90% of the way
-		case WhiteBird:
-			tapInterval = 70 + randomGenerator.nextInt(20);
-			break; // 70-90% of the way
-		case BlackBird:
-			tapInterval = 70 + randomGenerator.nextInt(20);
-			break; // 70-90% of the way
-		case BlueBird:
-			tapInterval = 65 + randomGenerator.nextInt(20);
-			break; // 65-85% of the way
-		default:
-			tapInterval = 60;
-		}
-		return tp.getTapTime(sling, releasePoint, _tpt, tapInterval);
-	}
-
-	private double distance(Point p1, Point p2) {
-		return Math
-				.sqrt((double) ((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y)
-						* (p1.y - p2.y)));
-	}
-
-	public Shot createShot(ABObject abObject, Rectangle sling, Point _tpt,
-			Point releasePoint) {
-		Shot shot = null;
-
-		// point near it
-		if (prevTarget != null && distance(prevTarget, _tpt) < 10) {
-			double _angle = randomGenerator.nextDouble() * Math.PI * 2;
-			_tpt.x = _tpt.x + (int) (Math.cos(_angle) * 10);
-			_tpt.y = _tpt.y + (int) (Math.sin(_angle) * 10);
-			System.out.println("Randomly changing to " + _tpt);
-		}
-
-		prevTarget = new Point(_tpt.x, _tpt.y);
-
-		// Get the reference point
-		Point refPoint = tp.getReferencePoint(sling);
-
-		// Calculate the tapping time according the bird type
-		if (releasePoint != null) {
-			int tapTime = getTapTime(sling, releasePoint, _tpt);
-			int dx = (int) releasePoint.getX() - refPoint.x;
-			int dy = (int) releasePoint.getY() - refPoint.y;
-
-			shot = new Shot(refPoint.x, refPoint.y, dx, dy, 0, tapTime,
-					releasePoint);
-		}
-
-		return shot;
-	}
-
-	public GameState executeShot(Rectangle sling, Shot shot, GameState state,
-			Point releasePoint) {
-		// check whether the slingshot is changed. the change of the slingshot
-		// indicates a change in the scale.
-		ActionRobot.fullyZoomOut();
-		BufferedImage screenshot = ActionRobot.doScreenShot();
-		Vision vision = new Vision(screenshot);
-		Rectangle _sling = vision.findSlingshotMBR();
-		if (_sling != null) {
-			double scale_diff = Math.pow((sling.width - _sling.width), 2)
-					+ Math.pow((sling.height - _sling.height), 2);
-			if (scale_diff < 25) {
-				if (shot.getDx() < 0) {
-					aRobot.cshoot(shot);
-					listObjects.add(shot);
-					shotNumber++;
-					state = aRobot.getState();
-
-					if (state == GameState.PLAYING) {
-						screenshot = ActionRobot.doScreenShot();
-						vision = new Vision(screenshot);
-						List<Point> traj = vision.findTrajPoints();
-						tp.adjustTrajectory(traj, sling, releasePoint);
-						firstShot = false;
-					}
-				}
-			} else {
-				System.out
-						.println("Scale is changed, can not execute the shot, will re-segement the image");
-			}
-		} else {
-			System.out
-					.println("no sling detected, can not execute the shot, will re-segement the image");
-		}
-		return state;
-	}
-
 	public static void main(String args[]) {
 		RandomAgent na = new RandomAgent();
 		if (args.length > 0) {
 			na.currentLevel = Integer.parseInt(args[0]);
 		}
 		na.run();
+	}
+	
+	private void saveBestShots() {
+		this.saveKnowledge(bestShots, BEST_SHOTS_FILE);
+	}
+	
+	private ListBestShots loadBestShots(){
+		return (ListBestShots) this.loadKnowledge(ListBestShots.class, BEST_SHOTS_FILE);
 	}
 }
