5feeb85a8a9581b542d59221ae5bc6abb38bbfc6
George Jegon <george_jegon@hotmail.com>
2015-10-08 00:54:38 -0300

    Created methods to loadXML from input String or from lines of a file

--- src/ab/ai/Util.java
+++ src/ab/ai/Util.java
@@ -5,20 +5,30 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.commons.codec.Charsets;
+
 public class Util {
 	// Save XML
-	public static void saveXML(Object object, String fileNamePath, Boolean append) {
+	public static void saveXML(Object object, String fileNamePath,
+			Boolean append) {
 		Marshaller marshaller = createMarshaller(object);
 		FileWriter fileWriter;
 		try {
 			fileWriter = new FileWriter(fileNamePath, append);
-			fileWriter.append(System.getProperty("line.separator"));
+			if (append) {
+				fileWriter.append(System.getProperty("line.separator"));
+			}
 			marshaller.marshal(object, fileWriter);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -28,7 +38,7 @@ public class Util {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void saveXML(Object object, String fileNamePath) {
 		saveXML(object, fileNamePath, false);
 	}
@@ -43,11 +53,47 @@ public class Util {
 		}
 	}
 
+	// Load XML
+	public static List<?> loadXMLFileByLine(Object object, String fileNamePath) {
+		List<Object> loaded = new ArrayList<Object>();
+		Object loadedObject = null;
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(fileNamePath),
+					Charsets.UTF_8);
+
+			for (String line : lines) {
+				loadedObject = Util.loadXMLFromString(object, line);
+				if (loadedObject != null) {
+					loaded.add(loadedObject);
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return loaded;
+	}
+
+	public static Object loadXMLFromString(Object object, String line) {
+		Unmarshaller unmarshaller = createUnmarshaller(object);
+		Object loaded = null;
+		StringReader stringReader = new StringReader(line);
+		if (!line.isEmpty()) {
+			try {
+				loaded = unmarshaller.unmarshal(stringReader);
+			} catch (JAXBException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return loaded;
+	}
+
 	public static Object loadXML(Object object, String fileNamePath) {
 		Unmarshaller unmarshaller = createUnmarshaller(object);
 		Object loaded = null;
 		File fileXML = new File(fileNamePath);
-		
+
 		if (fileXML.length() > 0) {
 			try {
 				loaded = unmarshaller.unmarshal(new FileReader(fileNamePath));
@@ -57,9 +103,9 @@ public class Util {
 			} catch (JAXBException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}	
+			}
 		}
-		
+
 		return loaded;
 	}
 
@@ -80,7 +126,7 @@ public class Util {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return unmarshaller;
 	}
 
