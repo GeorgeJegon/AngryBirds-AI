eb8b7a9ffce8e8500ff3e404188b02af763034c8
George Jegon <george_jegon@hotmail.com>
2015-10-20 02:22:16 -0200

    Added showTrajectory method to Agent class

--- src/ab/ai/agents/Agent.java
+++ src/ab/ai/agents/Agent.java
@@ -11,258 +11,273 @@ import ab.ai.Util;
 import ab.demo.other.ActionRobot;
 import ab.demo.other.Shot;
 import ab.planner.TrajectoryPlanner;
+import ab.utils.ImageSegFrame;
 import ab.vision.ABObject;
-import ab.vision.Vision;
 import ab.vision.GameStateExtractor.GameState;
+import ab.vision.Vision;
 
 public abstract class Agent {
-	protected ActionRobot aRobot;
-	protected Random randomGenerator;
+  private final String        SHOTS_FILE   = "src/ab/data/shots.xml";
+  private final String        MATCHES_FILE = "src/ab/data/matches.xml";
+  private ImageSegFrame       frame        = null;
 
-	protected TrajectoryPlanner tp;
-	protected boolean firstShot;
-	protected Point prevTarget;
+  protected ActionRobot       aRobot;
+  protected Random            randomGenerator;
 
-	private final String SHOTS_FILE = "src/ab/data/shots.xml";
-	private final String MATCHES_FILE = "src/ab/data/matches.xml";
+  protected TrajectoryPlanner tp;
+  protected boolean           firstShot;
+  protected Point             prevTarget;
 
-	public int currentLevel = 1;
-	public static int time_limit = 12;
-	public int sleepTime = 3000;
+  public int                  currentLevel = 1;
+  public static int           time_limit   = 12;
+  public int                  sleepTime    = 3000;
 
-	public Agent() {
-		this.aRobot = new ActionRobot();
-		this.tp = new TrajectoryPlanner();
-		this.prevTarget = null;
-		this.firstShot = true;
-		this.randomGenerator = new Random();
-	}
+  public Agent() {
+    this.aRobot = new ActionRobot();
+    this.tp = new TrajectoryPlanner();
+    this.prevTarget = null;
+    this.firstShot = true;
+    this.randomGenerator = new Random();
+  }
 
-	public void run() {
-		aRobot.loadLevel(currentLevel);
+  protected void showTrajectory(BufferedImage screenshot, Rectangle sling,
+      Point releasePoint) {
+    this.tp.plotTrajectory(screenshot, sling, releasePoint);
 
-		while (true) {
-			System.out.println("Execute level " + currentLevel);
+    if (this.frame == null) {
+      this.frame = new ImageSegFrame("trajectory", screenshot);
+    } else {
+      this.frame.refresh(screenshot);
+    }
+  }
 
-			GameState state = solve();
+  protected void showTrajectory(Rectangle sling, Point releasePoint) {
+    BufferedImage screenshot = ActionRobot.doScreenShot();
+    showTrajectory(screenshot, sling, releasePoint);
+  }
 
-			switch (state) {
-			case EPISODE_MENU:
-				this.onEpisodeMenuState();
-				break;
-			case LEVEL_SELECTION:
-				this.onLevelSelectionState();
-				break;
-			case LOADING:
-				break;
-			case LOST:
-				this.onLostState();
-				break;
-			case MAIN_MENU:
-				this.onMainMenuState();
-				break;
-			case PLAYING:
-				break;
-			case UNKNOWN:
-				break;
-			case WON:
-				this.onWonState();
-				break;
-			}
-		}
-	}
+  public void run() {
+    aRobot.loadLevel(currentLevel);
 
-	protected abstract GameState solve();
+    while (true) {
+      System.out.println("Execute level " + currentLevel);
 
-	protected abstract void beforeRestartLevel();
+      GameState state = solve();
 
-	protected abstract void afterRestartLevel();
+      switch (state) {
+      case EPISODE_MENU:
+        this.onEpisodeMenuState();
+        break;
+      case LEVEL_SELECTION:
+        this.onLevelSelectionState();
+        break;
+      case LOADING:
+        break;
+      case LOST:
+        this.onLostState();
+        break;
+      case MAIN_MENU:
+        this.onMainMenuState();
+        break;
+      case PLAYING:
+        break;
+      case UNKNOWN:
+        break;
+      case WON:
+        this.onWonState();
+        break;
+      }
+    }
+  }
 
-	protected abstract void beforeLoadNextLevel();
+  protected abstract GameState solve();
 
-	protected abstract void afterLoadNextLevel();
-	
-	protected abstract void onShotFinish(Shot currentShot);
+  protected abstract void beforeRestartLevel();
 
-	protected void saveKnowledge(Object object, String filePathName) {
-		Util.saveXML(object, filePathName);
-	}
+  protected abstract void afterRestartLevel();
 
-	@SuppressWarnings("rawtypes")
-	protected Object loadKnowledge(Class objectClass, String filePathName) {
-		Object loaded = objectClass.cast(Util
-				.loadXML(objectClass, filePathName));
-		Object loadedInstance = null;
-		try {
-			loadedInstance = objectClass.newInstance();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return (loaded != null) ? loaded : loadedInstance;
-	}
+  protected abstract void beforeLoadNextLevel();
 
-	protected Point getReleasePoint(Rectangle sling, Point _tpt) {
-		Point releasePoint = null;
-		// estimate the trajectory
-		ArrayList<Point> pts = tp.estimateLaunchPoint(sling, _tpt);
+  protected abstract void afterLoadNextLevel();
 
-		// do a high shot when entering a level to find an accurate velocity
-		if (firstShot && pts.size() > 1) {
-			releasePoint = pts.get(1);
-		} else if (pts.size() == 1) {
-			releasePoint = pts.get(0);
-		} else if (pts.size() == 2) {
-			// randomly choose between the trajectories, with a 1 in
-			// 6 chance of choosing the high one
-			if (randomGenerator.nextInt(6) == 0)
-				releasePoint = pts.get(1);
-			else
-				releasePoint = pts.get(0);
-		} else {
-			if (pts.isEmpty()) {
-				System.out.println("No release point found for the target");
-				System.out.println("Try a shot with 45 degree");
-				releasePoint = tp.findReleasePoint(sling, Math.PI / 4);
-			}
-		}
-		return releasePoint;
-	}
+  protected abstract void onShotFinish(Shot currentShot);
 
-	protected int getTapTime(Rectangle sling, Point releasePoint, Point _tpt) {
-		int tapInterval = 0;
-		switch (aRobot.getBirdTypeOnSling()) {
-		case RedBird:
-			tapInterval = 0;
-			break; // start of trajectory
-		case YellowBird:
-			tapInterval = 65 + randomGenerator.nextInt(25);
-			break; // 65-90% of the way
-		case WhiteBird:
-			tapInterval = 70 + randomGenerator.nextInt(20);
-			break; // 70-90% of the way
-		case BlackBird:
-			tapInterval = 70 + randomGenerator.nextInt(20);
-			break; // 70-90% of the way
-		case BlueBird:
-			tapInterval = 65 + randomGenerator.nextInt(20);
-			break; // 65-85% of the way
-		default:
-			tapInterval = 60;
-		}
-		return tp.getTapTime(sling, releasePoint, _tpt, tapInterval);
-	}
+  protected void saveKnowledge(Object object, String filePathName) {
+    Util.saveXML(object, filePathName);
+  }
 
-	protected GameState executeShot(Rectangle sling, Shot shot,
-			GameState state, Point releasePoint) {
-		// check whether the slingshot is changed. the change of the slingshot
-		// indicates a change in the scale.
-		ActionRobot.fullyZoomOut();
-		BufferedImage screenshot = ActionRobot.doScreenShot();
-		Vision vision = new Vision(screenshot);
-		Rectangle _sling = vision.findSlingshotMBR();
-		if (_sling != null) {
-			double scale_diff = Math.pow((sling.width - _sling.width), 2)
-					+ Math.pow((sling.height - _sling.height), 2);
-			if (scale_diff < 25) {
-				if (shot.getDx() < 0) {
-					aRobot.cshoot(shot);
-					this.onShotFinish(shot);
-					state = aRobot.getState();
+  @SuppressWarnings("rawtypes")
+  protected Object loadKnowledge(Class objectClass, String filePathName) {
+    Object loaded = objectClass.cast(Util.loadXML(objectClass, filePathName));
+    Object loadedInstance = null;
+    try {
+      loadedInstance = objectClass.newInstance();
+    } catch (InstantiationException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return (loaded != null) ? loaded : loadedInstance;
+  }
 
-					if (state == GameState.PLAYING) {
-						screenshot = ActionRobot.doScreenShot();
-						vision = new Vision(screenshot);
-						List<Point> traj = vision.findTrajPoints();
-						tp.adjustTrajectory(traj, sling, releasePoint);
-						firstShot = false;
-					}
-				}
-			} else {
-				System.out
-						.println("Scale is changed, can not execute the shot, will re-segement the image");
-			}
-		} else {
-			System.out
-					.println("no sling detected, can not execute the shot, will re-segement the image");
-		}
-		return state;
-	}
+  protected Point getReleasePoint(Rectangle sling, Point _tpt) {
+    Point releasePoint = null;
+    // estimate the trajectory
+    ArrayList<Point> pts = tp.estimateLaunchPoint(sling, _tpt);
 
-	protected Shot createShot(ABObject abObject, Rectangle sling, Point _tpt,
-			Point releasePoint) {
-		Shot shot = null;
+    // do a high shot when entering a level to find an accurate velocity
+    if (firstShot && pts.size() > 1) {
+      releasePoint = pts.get(1);
+    } else if (pts.size() == 1) {
+      releasePoint = pts.get(0);
+    } else if (pts.size() == 2) {
+      // randomly choose between the trajectories, with a 1 in
+      // 6 chance of choosing the high one
+      if (randomGenerator.nextInt(6) == 0)
+        releasePoint = pts.get(1);
+      else
+        releasePoint = pts.get(0);
+    } else {
+      if (pts.isEmpty()) {
+        System.out.println("No release point found for the target");
+        System.out.println("Try a shot with 45 degree");
+        releasePoint = tp.findReleasePoint(sling, Math.PI / 4);
+      }
+    }
+    return releasePoint;
+  }
 
-		// point near it
-		if (prevTarget != null && distance(prevTarget, _tpt) < 10) {
-			double _angle = randomGenerator.nextDouble() * Math.PI * 2;
-			_tpt.x = _tpt.x + (int) (Math.cos(_angle) * 10);
-			_tpt.y = _tpt.y + (int) (Math.sin(_angle) * 10);
-			System.out.println("Randomly changing to " + _tpt);
-		}
+  protected int getTapTime(Rectangle sling, Point releasePoint, Point _tpt) {
+    int tapInterval = 0;
+    switch (aRobot.getBirdTypeOnSling()) {
+    case RedBird:
+      tapInterval = 0;
+      break; // start of trajectory
+    case YellowBird:
+      tapInterval = 65 + randomGenerator.nextInt(25);
+      break; // 65-90% of the way
+    case WhiteBird:
+      tapInterval = 70 + randomGenerator.nextInt(20);
+      break; // 70-90% of the way
+    case BlackBird:
+      tapInterval = 70 + randomGenerator.nextInt(20);
+      break; // 70-90% of the way
+    case BlueBird:
+      tapInterval = 65 + randomGenerator.nextInt(20);
+      break; // 65-85% of the way
+    default:
+      tapInterval = 60;
+    }
+    return tp.getTapTime(sling, releasePoint, _tpt, tapInterval);
+  }
 
-		prevTarget = new Point(_tpt.x, _tpt.y);
+  protected GameState executeShot(Rectangle sling, Shot shot, GameState state,
+      Point releasePoint) {
+    // check whether the slingshot is changed. the change of the slingshot
+    // indicates a change in the scale.
+    ActionRobot.fullyZoomOut();
+    BufferedImage screenshot = ActionRobot.doScreenShot();
+    Vision vision = new Vision(screenshot);
+    Rectangle _sling = vision.findSlingshotMBR();
+    if (_sling != null) {
+      double scale_diff = Math.pow((sling.width - _sling.width), 2)
+          + Math.pow((sling.height - _sling.height), 2);
+      if (scale_diff < 25) {
+        if (shot.getDx() < 0) {
+          aRobot.cshoot(shot);
+          this.onShotFinish(shot);
+          state = aRobot.getState();
 
-		// Get the reference point
-		Point refPoint = tp.getReferencePoint(sling);
+          if (state == GameState.PLAYING) {
+            screenshot = ActionRobot.doScreenShot();
+            vision = new Vision(screenshot);
+            List<Point> traj = vision.findTrajPoints();
+            tp.adjustTrajectory(traj, sling, releasePoint);
+            firstShot = false;
+          }
+        }
+      } else {
+        System.out
+            .println("Scale is changed, can not execute the shot, will re-segement the image");
+      }
+    } else {
+      System.out
+          .println("no sling detected, can not execute the shot, will re-segement the image");
+    }
+    return state;
+  }
 
-		// Calculate the tapping time according the bird type
-		if (releasePoint != null) {
-			int tapTime = getTapTime(sling, releasePoint, _tpt);
-			int dx = (int) releasePoint.getX() - refPoint.x;
-			int dy = (int) releasePoint.getY() - refPoint.y;
+  protected Shot createShot(ABObject abObject, Rectangle sling, Point _tpt,
+      Point releasePoint) {
+    Shot shot = null;
 
-			shot = new Shot(refPoint.x, refPoint.y, dx, dy, 0, tapTime,
-					releasePoint);
-		}
+    // point near it
+    if (prevTarget != null && distance(prevTarget, _tpt) < 10) {
+      double _angle = randomGenerator.nextDouble() * Math.PI * 2;
+      _tpt.x = _tpt.x + (int) (Math.cos(_angle) * 10);
+      _tpt.y = _tpt.y + (int) (Math.sin(_angle) * 10);
+      System.out.println("Randomly changing to " + _tpt);
+    }
 
-		return shot;
-	}
+    prevTarget = new Point(_tpt.x, _tpt.y);
 
-	protected double distance(Point p1, Point p2) {
-		return Math
-				.sqrt((double) ((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y)
-						* (p1.y - p2.y)));
-	}
+    // Get the reference point
+    Point refPoint = tp.getReferencePoint(sling);
 
-	protected void onLostState() {
-		System.out.println("Restart");
-		aRobot.restartLevel();
-	}
+    // Calculate the tapping time according the bird type
+    if (releasePoint != null) {
+      int tapTime = getTapTime(sling, releasePoint, _tpt);
+      int dx = (int) releasePoint.getX() - refPoint.x;
+      int dy = (int) releasePoint.getY() - refPoint.y;
 
-	private void onEpisodeMenuState() {
-		System.out.println("Unexpected episode menu page: " + currentLevel);
-		ActionRobot.GoFromMainMenuToLevelSelection();
-		this.aRobot.loadLevel(currentLevel);
-	}
+      shot = new Shot(refPoint.x, refPoint.y, dx, dy, 0, tapTime, releasePoint);
+    }
 
-	private void onMainMenuState() {
-		System.out.println("Unexpected main menu page: " + currentLevel);
-		ActionRobot.GoFromMainMenuToLevelSelection();
-		aRobot.loadLevel(currentLevel);
-	}
+    return shot;
+  }
 
-	private void onLevelSelectionState() {
-		System.out.println("Unexpected level selection page: " + currentLevel);
-		aRobot.loadLevel(currentLevel);
-	}
+  protected double distance(Point p1, Point p2) {
+    return Math.sqrt((double) ((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y)
+        * (p1.y - p2.y)));
+  }
 
-	private void onWonState() {
-		this.beforeLoadNextLevel();
+  protected void onLostState() {
+    System.out.println("Restart");
+    aRobot.restartLevel();
+  }
 
-		try {
-			Thread.sleep(this.sleepTime);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+  private void onEpisodeMenuState() {
+    System.out.println("Unexpected episode menu page: " + currentLevel);
+    ActionRobot.GoFromMainMenuToLevelSelection();
+    this.aRobot.loadLevel(currentLevel);
+  }
 
-		aRobot.loadLevel(++currentLevel);
-		tp = new TrajectoryPlanner();
-		firstShot = true;
+  private void onMainMenuState() {
+    System.out.println("Unexpected main menu page: " + currentLevel);
+    ActionRobot.GoFromMainMenuToLevelSelection();
+    aRobot.loadLevel(currentLevel);
+  }
 
-		this.afterLoadNextLevel();
-	}
+  private void onLevelSelectionState() {
+    System.out.println("Unexpected level selection page: " + currentLevel);
+    aRobot.loadLevel(currentLevel);
+  }
+
+  private void onWonState() {
+    this.beforeLoadNextLevel();
+
+    try {
+      Thread.sleep(this.sleepTime);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    aRobot.loadLevel(++currentLevel);
+    tp = new TrajectoryPlanner();
+    firstShot = true;
+
+    this.afterLoadNextLevel();
+  }
 }
