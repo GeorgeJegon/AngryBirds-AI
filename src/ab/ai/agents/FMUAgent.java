6c4f1e90fcc8ce49c58c0b24e8bcd1d4010fa3e4
George Jegon <george_jegon@hotmail.com>
2015-10-26 23:07:14 -0200

    Apply Heuristic and save match with final GameState (WON or LOST)

--- src/ab/ai/agents/FMUAgent.java
+++ src/ab/ai/agents/FMUAgent.java
@@ -3,20 +3,19 @@ package ab.ai.agents;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.util.LinkedList;
-import java.util.List;
 
+import ab.ai.Match;
+import ab.ai.Util;
+import ab.ai.Heuristics.Heuristic;
+import ab.ai.Heuristics.RandomObject;
 import ab.demo.other.ActionRobot;
 import ab.demo.other.Shot;
-import ab.planner.abTrajectory;
-import ab.utils.ImageSegFrame;
 import ab.vision.ABObject;
 import ab.vision.GameStateExtractor.GameState;
 import ab.vision.Vision;
-import ab.vision.VisionMBR;
 
 public class FMUAgent extends Agent implements Runnable {
-  public ImageSegFrame frame = null;
+  private static final String MATCHES_FILE = "src/ab/data/FMUAgent/matches.xml";
 
   public FMUAgent() {
     super();
@@ -30,31 +29,60 @@ public class FMUAgent extends Agent implements Runnable {
 
   @Override
   protected GameState solve() {
-    GameState state = GameState.PLAYING;
+    this.aRobot.click();
+    ActionRobot.fullyZoomOut();
+    GameState state = this.aRobot.getState();
+    Heuristic heuristic = new RandomObject();
 
     BufferedImage screenshot = ActionRobot.doScreenShot();
     Vision vision = new Vision(screenshot);
     Rectangle sling = vision.findSlingshotMBR();
-    List<ABObject> listPigs = vision.findPigsMBR();
+    Shot shot = null;
+    ABObject target = null;
+    Point objectCenter = null;
+    Point releasePoint = null;
 
-    ABObject pig = listPigs.get(0);
-    Point _tpt = pig.getCenter();
-    Point releasePoint = getReleasePoint(sling, _tpt);
+    if (this.waitForSling(vision, sling, screenshot)) {
+      state = this.aRobot.getState();
 
-    this.showTrajectory(sling, releasePoint);
 
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
+      target = heuristic.solve(vision);
+      objectCenter = target.getCenter();
+      releasePoint = getReleasePoint(sling, objectCenter);
+
+      shot = createShot(target, sling, objectCenter, releasePoint);
+
+      if (shot == null) {
+        System.err.println("No Release Point Found");
+        return state;
+      }
+
+      state = executeShot(sling, shot, state, releasePoint);
     }
 
     return state;
   }
 
+  protected void saveMatch(GameState state) {
+    Match match = new Match();
+    int score = this.aRobot.current_score;
+    
+    if (state == GameState.WON) {
+      score = this.aRobot.getScore();
+    }
+
+    match.setLevel(this.currentLevel);
+    match.setScore(score);
+    match.setShots(this.listShots);
+    match.setType(state.toString());
+    
+    Util.saveXML(match, MATCHES_FILE, true);
+  }
+
   @Override
   protected void beforeRestartLevel() {
     // TODO Auto-generated method stub
-
+    this.saveMatch(GameState.LOST);
   }
 
   @Override
@@ -66,7 +94,7 @@ public class FMUAgent extends Agent implements Runnable {
   @Override
   protected void beforeLoadNextLevel() {
     // TODO Auto-generated method stub
-
+    this.saveMatch(GameState.WON);
   }
 
   @Override
@@ -77,6 +105,12 @@ public class FMUAgent extends Agent implements Runnable {
 
   @Override
   protected void onShotFinish(Shot currentShot) {
+    int current_score = this.aRobot.current_score;
+    int shot_score = this.aRobot.getScore() - current_score;
 
+    currentShot.setScore(shot_score);
+    this.listShots.add(currentShot);
+
+    this.aRobot.current_score += shot_score;
   }
 }
