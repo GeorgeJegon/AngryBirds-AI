1fea21e761f7fd03520d6eb675fc269def46f70e
George Jegon <george_jegon@hotmail.com>
2015-11-17 14:52:14 -0200

    Added method to filter Building Components

--- src/ab/ai/Building.java
+++ src/ab/ai/Building.java
@@ -3,7 +3,9 @@ package ab.ai;
 import java.util.ArrayList;
 import java.util.List;
 
+import ab.utils.ABUtil;
 import ab.vision.ABObject;
+import ab.vision.ABType;
 
 public class Building {
   private List<ABObject> components = new ArrayList<ABObject>();
@@ -14,5 +16,9 @@ public class Building {
 
   public List<ABObject> getComponents() {
     return this.components;
+  }
+
+  public List<ABObject> filterComponets(ABType type) {
+    return ABUtil.filterObjects(this.components, type);
   }
 }
