260a2cad6487d0985534ef766e9d8cd6775b583b
George Jegon <george_jegon@hotmail.com>
2015-11-30 06:12:53 -0200

    Refactoring Heuristic abstract class

--- src/ab/ai/Heuristics/Heuristic.java
+++ src/ab/ai/Heuristics/Heuristic.java
@@ -1,27 +1,37 @@
 package ab.ai.Heuristics;
 
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
+import ab.ai.agents.Agent;
 import ab.vision.ABObject;
+import ab.vision.ABType;
+import ab.vision.GameStateExtractor.GameState;
 import ab.vision.Vision;
 
 @XmlRootElement
-@XmlType(propOrder = { "name", "frequency"})
+@XmlType(propOrder = { "name", "frequency" })
 public abstract class Heuristic {
   private int             frequency;
   private int             badDecreaseRate;
+  protected Random        randomGenerator;
 
   public final static int HEURISTIC_VALUE = 100;
 
   public Heuristic() {
     this.frequency = HEURISTIC_VALUE;
     this.badDecreaseRate = 1;
+    this.randomGenerator = new Random();
   }
 
-  public abstract ABObject solve(Vision vision);
+  public abstract GameState solve(Agent agent, GameState state, Vision vision,
+      Rectangle sling, BufferedImage screenshot, ABType birdOnSling);
 
   public void bad() {
     bad(this.badDecreaseRate);
@@ -48,7 +58,7 @@ public abstract class Heuristic {
   public void setBadDecreaseRate(int badDecreaseRate) {
     this.badDecreaseRate = badDecreaseRate;
   }
-  
+
   @XmlAttribute
   public String getName() {
     return this.getClass().getSimpleName();
